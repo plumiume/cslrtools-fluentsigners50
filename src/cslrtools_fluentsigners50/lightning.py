a659fb58f112d2f4d93ae18e48be3fd00986f180
ikeda <ikeda@lee-lab.org>
2025-06-09 20:44:40 +0900

    add load_or_create_cross_validation_groups

--- src/cslrtools_fluentsigners50/lightning.py
+++ src/cslrtools_fluentsigners50/lightning.py
@@ -6,6 +6,8 @@ except ImportError:
         f"Please install it with 'pip install {__package__}[lightning]'"
     )
 
+import pickle
+from pathlib import Path
 import torch
 from cslrtools.dataset.pytorch import Dataset
 from . import FluentSigners50Metadata
@@ -20,6 +22,23 @@ class FluentSigners50DataModule(LightningDataModule[FluentSigners50Metadata]):
         common_kwargs: LightningDataModule.DataLoaderCommonKwargs = {}
         ):
         super().__init__(dataset, stages, common_kwargs)
+
+    @classmethod
+    def load_or_create_cross_validataion_groups(
+        cls,
+        groups_pkl: Path,
+        dataset: Dataset[FluentSigners50Metadata],
+        num_splits: int = 5,
+        ):
+
+        if groups_pkl.exists():
+            groups: torch.Tensor = pickle.load(groups_pkl.open('rb'))
+        else:
+            people = set(m['person'] for m in dataset._metas)
+            groups = torch.rand((len(people),)).argsort() % num_splits
+            pickle.dump(groups, groups_pkl.open('wb'))
+
+        return groups
 
     @classmethod
     def generate_cross_validation_splits_by_person(
